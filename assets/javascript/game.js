d715f371b50bfc5d61c2a8a21adbf5cd8cd55127
louisqvo <louisqvo@gmail.com>
2018-06-14 13:50:19 -0400

    fix all the bugs and add some css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,6 +25,8 @@ var character = [
     }
 ];
 
+var charPick = true;
+var enePick = true;
 var pick = 5;
 var opp = 5;
 var winner = 0;
@@ -34,68 +36,80 @@ $(document).ready(function() {
     initial();
 
     $(".char").on("click", function(){
-
-        $(".char").hide();
-        $(this).show();
-        $(".enemy").show();
-
-        pick = parseInt($(this).attr("value"));
-
-        for(var i = 0; i < character.length; i++){
-            if($(this).attr("value") === $(".enemy" + i).attr("value")){
-               $(".enemy" + i).hide();
+        if(charPick){
+            $(".char").hide();
+            $(this).show();
+            $(".enemy").show();
+    
+            pick = parseInt($(this).attr("value"));
+    
+            for(var i = 0; i < character.length; i++){
+                if($(this).attr("value") === $(".enemy" + i).attr("value")){
+                   $(".enemy" + i).hide();
+                }
             }
+            charPick = false;
         }
     });
 
     $(".enemy").on("click", function(){
-        $(this).hide();
+        if(enePick){
+            $(this).hide();
 
-        opp = parseInt($(this).attr("value"));
-
-        for(var i = 0; i < character.length; i++){
-            if($(this).attr("value") === $(".fighter" + i).attr("value")){
-               $(".fighter" + i).show();
+            opp = parseInt($(this).attr("value"));
+    
+            for(var i = 0; i < character.length; i++){
+                if($(this).attr("value") === $(".fighter" + i).attr("value")){
+                   $(".fighter" + i).show();
+                }
             }
+            enePick = false;
         }
     });
 
+    var attBtn = true;
+
     $("#attack").on("click", function(){
-
-        if(pick === 5 || opp === 5){
-            $("#yourMove").text("No enemy here.")
-        }else{
-            character[opp].health -= character[pick].power;
-
-            if(character[opp].health <= 0){
-                winner++;
-                $("#yourMove").text("You have defeated " + character[opp].name + ", you can choose to fight another enemy.");
-                $("#oppMove").text("");
-                $(".fighter" + opp).hide();
-                if(winner === 3){
-                    $("#yourMove").text("You Won!!! GAME OVER!!!")
-                    $("#restart").show();
-                }
-                opp = 5;
-
+        if(attBtn){
+            if(pick === 5 || opp === 5 ){
+                $("#yourMove").text("No enemy here.")
             }else{
-                character[pick].health -= character[opp].counter;
-
-                $(".btn" + pick).text(character[pick].name + " " + character[pick].health);
-
-                $(".fighter" + opp).text(character[opp].name + " " +  character[opp].health);
-
-                if(character[pick].health <= 0){
-                    $("#yourMove").text("You been defeated...GAME OVER!!!");
+                character[opp].health -= character[pick].power;
+    
+                if(character[opp].health <= 0){
+                    winner++;
+                    $("#yourMove").text("You have defeated " + character[opp].name + ", you can choose to fight another enemy.");
                     $("#oppMove").text("");
-                    $("#restart").show();
+                    $(".fighter" + opp).hide();
+                    enePick = true;
+                    if(winner === 3){
+                        $("#yourMove").text("You Won!!! GAME OVER!!!")
+                        $("#restart").show();
+                    }
+                    opp = 5;
+    
                 }else{
-                    $("#yourMove").text("you attacked " + character[opp].name + " for " + character[pick].power + " damage");
-                    $("#oppMove").text(character[opp].name + " attacked you back for " + character[opp].counter + " damage");
+                    character[pick].health -= character[opp].counter;
+    
+                    $(".btn" + pick).text(character[pick].name + " " + character[pick].health);
+                    $(".btn" + pick).append("<img src = assets/images/" + character[pick].name + ".jpg>");
+    
+                    $(".fighter" + opp).text(character[opp].name + " " +  character[opp].health);
+                    $(".fighter" + opp).append("<img src = assets/images/" + character[opp].name + ".jpg>");
+
+                    if(character[pick].health <= 0){
+                        $("#yourMove").text("You been defeated...GAME OVER!!!");
+                        $("#oppMove").text("");
+                        $("#restart").show();
+                        attBtn = false;
+                    }else{
+                        $("#yourMove").text("you attacked " + character[opp].name + " for " + character[pick].power + " damage");
+                        $("#oppMove").text(character[opp].name + " attacked you back for " + character[opp].counter + " damage");
+                    }
+                    character[pick].power += 8;
                 }
-                character[pick].power += 8;
             }
-        }
+        } 
     });
 
     $("#restart").click(function(){
@@ -114,15 +128,20 @@ $(document).ready(function() {
         character[pick].power = 8;
 
         $(".btn" + pick).text(character[pick].name + " " + character[pick].health);
+        $(".btn" + pick).append("<img src = assets/images/" + character[pick].name + ".jpg>");
 
         for(var i = 0; i < character.length; i++){
             if(i !== pick){
                 $(".fighter" + i).text(character[i].name + " " +  character[i].health);
+                $(".fighter" + i).append("<img src = assets/images/" + character[i].name + ".jpg>");
             }
         }
         pick = 5;
         opp = 5;
         winner = 0;
+        attBtn = true;
+        charPick = true;
+        enePick = true;
     });
  
 });
@@ -139,6 +158,8 @@ function initial(){
     
         charBtn.text(character[i].name + " " + character[i].health);
         
+        charBtn.append("<img src = assets/images/" + character[i].name + ".jpg>");
+
         $("#you").append(charBtn);
     }
 
@@ -152,6 +173,9 @@ function initial(){
         charBtn.attr("value", i);
 
         charBtn.text(character[i].name + " " + character[i].health);
+
+        charBtn.append("<img src = assets/images/" + character[i].name + ".jpg>");
+
         
         $("#enemies").append(charBtn);
     }
@@ -165,6 +189,8 @@ function initial(){
         charBtn.attr("value", i);
 
         charBtn.text(character[i].name + " " + character[i].health);
+
+        charBtn.append("<img src = assets/images/" + character[i].name + ".jpg>");
         
         $("#defender").append(charBtn);
     }
